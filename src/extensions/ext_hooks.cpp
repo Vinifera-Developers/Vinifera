c38867859b5c113dd978b73576d76f096853cdc0
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-06-12 16:20:15 +0100

    Fixes a bug where "IsInsignificant" was not checked when a unit dies.

--- src/extensions/ext_hooks.cpp
+++ src/extensions/ext_hooks.cpp
@@ -68,6 +68,7 @@
 #include "houseext_hooks.h"
 #include "teamext_hooks.h"
 #include "factoryext_hooks.h"
+#include "footext_hooks.h"
 
 #include "dropshipext_hooks.h"
 
@@ -126,6 +127,7 @@ void Extension_Hooks()
 	HouseClassExtension_Hooks();
     TeamClassExtension_Hooks();
 	FactoryClassExtension_Hooks();
+    FootClassExtension_Hooks();
 
     DropshipExtension_Hooks();
 
