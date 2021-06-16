0a26e3054e8b7afeeeca8efd93273bbb6b1cc57a
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-06-16 16:04:14 +0100

    Fixes a couple of bugs where the Jumpjet was being set the wrong DoType when hovering between firing rounds in the air.

--- src/extensions/infantry/infantryext_hooks.cpp
+++ src/extensions/infantry/infantryext_hooks.cpp
@@ -27,12 +27,153 @@
  ******************************************************************************/
 #include "houseext_hooks.h"
 #include "infantry.h"
+#include "infantrytype.h"
 #include "fatal.h"
 #include "debughandler.h"
 #include "asserthandler.h"
 
 #include "hooker.h"
 #include "hooker_macros.h"
+
+
+/**
+ *  #issue-80
+ * 
+ *  Fixes the bug where the Jumpjet uses the wrong DoType when idle on the
+ *  ground. This was because the original code did not check if the infantry
+ *  was actually in the air or not and always assumed it was on the ground, as
+ *  a result it was always setting DO_STAND_READY.
+ * 
+ *  @author: CCHyper
+ */
+DECLARE_PATCH(_InfantryClass_Doing_AI_JumpJet_Idle_Patch)
+{
+    GET_REGISTER_STATIC(InfantryClass *, this_ptr, esi);
+    static const InfantryTypeClass *infantrytype;
+
+    infantrytype = reinterpret_cast<const InfantryTypeClass *>(this_ptr->Class_Of());
+
+    /**
+     *  Stolen code.
+     * 
+     *  If infantry is prone, set DO_PRONE.
+     */
+    if (this_ptr->IsProne) {
+        JMP(0x004D8B12);
+    }
+
+    if (infantrytype->IsJumpJet) {
+
+        /**
+         *  This is a Jumpjet infantry, make sure its in the air before
+         *  assigning the hover graphic sequence.
+         */
+        if (this_ptr->In_Air()) {
+            this_ptr->Do_Action(DO_HOVER, true);
+
+        } else {
+            this_ptr->Do_Action(DO_STAND_READY, true);
+        }
+
+    /**
+     *  Handle normal infantry.
+     */
+    } else {
+        this_ptr->Do_Action(DO_STAND_READY, true);
+    }
+
+    JMP(0x004D8CA1);
+}
+
+
+/**
+ *  #issue-80
+ * 
+ *  Fixes the bug where the Jumpjet uses the wrong DoType when on the
+ *  ground and in between firing rounds. This was because the original code
+ *  did not check if the infantry was actually in the air and assumed it always
+ *  is, thus setting DO_STAND_READY.
+ * 
+ *  @warning: This patch is within a branch that has already checked if the
+ *            infantry is firing!
+ * 
+ *  @author: CCHyper
+ */
+DECLARE_PATCH(_InfantryClass_AI_JumpJet_Idle_Between_Firing_Patch)
+{
+    GET_REGISTER_STATIC(InfantryClass *, this_ptr, esi);
+    static const InfantryTypeClass *infantrytype;
+
+    infantrytype = reinterpret_cast<const InfantryTypeClass *>(this_ptr->Class_Of());
+
+    if (infantrytype->IsJumpJet) {
+
+        /**
+         *  This is a Jumpjet infantry, make sure its in the air before
+         *  assigning the hover graphic sequence.
+         */
+        if (this_ptr->In_Air()) {
+            this_ptr->Do_Action(DO_HOVER);
+
+        } else {
+            this_ptr->Do_Action(DO_STAND_READY);
+        }
+
+    /**
+     *  Handle normal infantry.
+     */
+    } else {
+        this_ptr->Do_Action(DO_STAND_READY);
+    }
+
+    /**
+     *  Stolen code.
+     * 
+     *  Clear the firing flag.
+     */
+    this_ptr->IsFiring = false;
+
+    JMP(0x004D50E0);
+}
+
+
+/**
+ *  #issue-80
+ * 
+ *  Fixes the bug where the Jumpjet uses the wrong DoType when not moving
+ *  but actually in the air.
+ * 
+ *  @author: CCHyper
+ */
+DECLARE_PATCH(_InfantryClass_Movement_AI_JumpJet_Not_Moving_Patch)
+{
+    GET_REGISTER_STATIC(InfantryClass *, this_ptr, ebp);
+    static const InfantryTypeClass *infantrytype;
+
+    infantrytype = reinterpret_cast<const InfantryTypeClass *>(this_ptr->Class_Of());
+
+    if (infantrytype->IsJumpJet) {
+
+        /**
+         *  This is a Jumpjet infantry, make sure its in the air before
+         *  assigning the hover graphic sequence.
+         */
+        if (this_ptr->In_Air()) {
+            this_ptr->Do_Action(DO_HOVER);
+
+        } else {
+            this_ptr->Do_Action(DO_STAND_READY);
+        }
+
+    /**
+     *  Handle normal infantry.
+     */
+    } else {
+        this_ptr->Do_Action(DO_STAND_READY);
+    }
+
+    JMP(0x004D9087);
+}
 
 
 /**
@@ -70,4 +211,7 @@ DECLARE_PATCH(_InfantryClass_Firing_AI_JumpJet_In_Air_Patch)
 void InfantryClassExtension_Hooks()
 {
     Patch_Jump(0x004D88FA, &_InfantryClass_Firing_AI_JumpJet_In_Air_Patch);
+    Patch_Jump(0x004D8C83, &_InfantryClass_Doing_AI_JumpJet_Idle_Patch);
+    Patch_Jump(0x004D50C9, &_InfantryClass_AI_JumpJet_Idle_Between_Firing_Patch);
+    Patch_Jump(0x004D9076, &_InfantryClass_Movement_AI_JumpJet_Not_Moving_Patch);
 }
