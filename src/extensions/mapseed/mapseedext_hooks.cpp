d654c0fc6416c0d8e0418c82652790b5933dbad9
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-07-06 13:19:35 +0100

    Fixes various crashes with the random map generator if the "Neutral" or "Special" HouseTypes are not found.

--- src/extensions/mapseed/mapseedext_hooks.cpp
+++ src/extensions/mapseed/mapseedext_hooks.cpp
@@ -39,6 +39,208 @@
 
 
 /**
+ *  #issue-496
+ * 
+ *  Sanity check to make sure HouseType "Neutral" exists before using it.
+ * 
+ *  @author: CCHyper
+ */
+DECLARE_PATCH(_MapSeedClass_Generate_Place_Units_And_Infantry_Neutral_House_Crash_Fix)
+{
+    static const HouseTypeClass *housetype;
+    static HousesType house;
+    static HouseClass *hptr;
+
+    /**
+     *  Stolen bytes/code.
+     */
+    _asm { mov [esp+0x2C], esi }
+
+    house = HouseTypeClass::From_Name("Neutral");
+    hptr = HouseClass::As_Pointer(house);
+
+    /**
+     *  Make sure the house exists before placing the bridge repair hut.
+     */
+    if (!hptr) {
+        DEBUG_WARNING("Unable to find house \"%s\"!\n", "Neutral");
+        goto function_return;
+    }
+
+continue_function:
+    _asm { mov eax, hptr }
+    JMP_REG(ebx, 0x0054E7F3);
+
+function_return:
+    JMP_REG(ebx, 0x0054EB6D);
+}
+
+DECLARE_PATCH(_MapSeedClass_Generate_Place_Town_Buildings_Neutral_House_Crash_Fix)
+{
+    static const HouseTypeClass *housetype;
+    static HousesType house;
+    static HouseClass *hptr;
+
+    house = HouseTypeClass::From_Name("Neutral");
+    hptr = HouseClass::As_Pointer(house);
+
+    /**
+     *  Make sure the house exists before placing the bridge repair hut.
+     */
+    if (!hptr) {
+        DEBUG_WARNING("Unable to find house \"%s\"!\n", "Neutral");
+        goto function_return;
+    }
+
+continue_function:
+    _asm { mov eax, hptr }
+    JMP_REG(ebp, 0x0054E4A9);
+
+function_return:
+    JMP_REG(ecx, 0x0054E79C);
+}
+
+DECLARE_PATCH(_MapSeedClass_Generate_Place_Town_Infantry_Neutral_House_Crash_Fix)
+{
+    static const HouseTypeClass *housetype;
+    static HousesType house;
+    static HouseClass *hptr;
+
+    /**
+     *  Stolen bytes/code.
+     */
+    _asm { mov [esp+0x2C], esi }
+
+    house = HouseTypeClass::From_Name("Neutral");
+    hptr = HouseClass::As_Pointer(house);
+
+    /**
+     *  Make sure the house exists before placing the bridge repair hut.
+     */
+    if (!hptr) {
+        DEBUG_WARNING("Unable to find house \"%s\"!\n", "Neutral");
+        goto function_return;
+    }
+
+place_building:
+    _asm { mov eax, hptr }
+    JMP_REG(ebx, 0x0054C716);
+
+function_return:
+    JMP_REG(ebx, 0x0054CA6A);
+}
+
+DECLARE_PATCH(_MapSeedClass_Generate_Place_City_Buildings_Neutral_House_Crash_Fix)
+{
+    static const HouseTypeClass *housetype;
+    static HousesType house;
+    static HouseClass *hptr;
+
+    house = HouseTypeClass::From_Name("Neutral");
+    hptr = HouseClass::As_Pointer(house);
+
+    /**
+     *  Make sure the house exists before placing the bridge repair hut.
+     */
+    if (!hptr) {
+        DEBUG_WARNING("Unable to find house \"%s\"!\n", "Neutral");
+        goto function_return;
+    }
+
+continue_function:
+    _asm { mov eax, hptr }
+    JMP_REG(ecx, 0x0054C32D);
+
+function_return:
+    JMP_REG(ecx, 0x0054C6C2);
+}
+
+DECLARE_PATCH(_MapSeedClass_Generate_Place_Tiberium_Wildlife_Neutral_House_Crash_Fix)
+{
+    static const HouseTypeClass *housetype;
+    static HousesType house;
+    static HouseClass *hptr;
+
+    /**
+     *  Stolen bytes/code.
+     */
+    _asm { mov [esp+0x58], eax }
+
+    house = HouseTypeClass::From_Name("Neutral");
+    hptr = HouseClass::As_Pointer(house);
+
+    /**
+     *  Make sure the house exists before placing the bridge repair hut.
+     */
+    if (!hptr) {
+        DEBUG_WARNING("Unable to find house \"%s\"!\n", "Neutral");
+        goto return_false;
+    }
+
+continue_function:
+    _asm { mov eax, hptr }
+    JMP_REG(ecx, 0x00546A60);
+
+return_false:
+    JMP_REG(ecx, 0x005471A1);
+}
+
+DECLARE_PATCH(_MapSeedClass_Generate_Bridge_Hut_Neutral_House_Crash_Fix)
+{
+    static const HouseTypeClass *housetype;
+    static HousesType house;
+    static HouseClass *hptr;
+
+    house = HouseTypeClass::From_Name("Neutral");
+    hptr = HouseClass::As_Pointer(house);
+
+    /**
+     *  Make sure the house exists before placing the bridge repair hut.
+     */
+    if (!hptr) {
+        DEBUG_WARNING("Unable to find house \"%s\"!\n", "Neutral");
+        goto return_false;
+    }
+
+place_building:
+    _asm { mov ecx, hptr }
+    JMP_REG(edi, 0x00535445);
+
+return_false:
+    JMP_REG(edi, 0x00535428);    
+}
+
+
+/**
+ *  #issue-496
+ * 
+ *  Sanity check to make sure HouseType "Special" exists before using it.
+ * 
+ *  @author: CCHyper
+ */
+DECLARE_PATCH(_MapSeedClass_Generate_Add_Lights_Special_House_Crash_Fix)
+{
+    static const HouseTypeClass *housetype;
+    static HousesType house;
+    static HouseClass *hptr;
+
+    house = HouseTypeClass::From_Name("Special");
+    hptr = HouseClass::As_Pointer(house);
+
+    /**
+     *  Make sure the house exists before placing the bridge repair hut.
+     */
+    if (!hptr) {
+        DEBUG_WARNING("Unable to find house \"%s\"!\n", "Special");
+        //goto return_false;
+    }
+
+    _asm { mov ecx, hptr }
+    JMP_REG(ecx, 0x0054F42E);
+}
+
+
+/**
  *  Initialises all houses for the Random Map Generator.
  * 
  *  @author: CCHyper
@@ -92,4 +294,11 @@ DECLARE_PATCH(_MapSeedClass_Init_Random_Map_Init_Houses_Patch)
 void MapSeedClassExtension_Hooks()
 {
     Patch_Jump(0x0053E48B, &_MapSeedClass_Init_Random_Map_Init_Houses_Patch);
+    Patch_Jump(0x0054F41D, &_MapSeedClass_Generate_Add_Lights_Special_House_Crash_Fix);
+    Patch_Jump(0x00535434, &_MapSeedClass_Generate_Bridge_Hut_Neutral_House_Crash_Fix);
+    Patch_Jump(0x00546A4B, &_MapSeedClass_Generate_Place_Tiberium_Wildlife_Neutral_House_Crash_Fix);
+    Patch_Jump(0x0054C31C, &_MapSeedClass_Generate_Place_City_Buildings_Neutral_House_Crash_Fix);
+    Patch_Jump(0x0054C701, &_MapSeedClass_Generate_Place_Town_Infantry_Neutral_House_Crash_Fix);
+    Patch_Jump(0x0054E498, &_MapSeedClass_Generate_Place_Town_Buildings_Neutral_House_Crash_Fix);
+    Patch_Jump(0x0054E7DE, &_MapSeedClass_Generate_Place_Units_And_Infantry_Neutral_House_Crash_Fix);
 }
