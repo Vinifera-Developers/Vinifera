044c66c63abb1aafcb48bc6c3f26ca418ed79c6a
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-09-17 00:11:17 +0100

    Implements ReduceTiberiumCommandClass.

--- src/extensions/command/commandext.h
+++ src/extensions/command/commandext.h
@@ -1007,6 +1007,25 @@ class PlaceTiberiumCommandClass : public ViniferaCommandClass
         virtual KeyNumType Default_Key() const override { return KeyNumType(KN_NONE); }
 };
 
+/**
+ *  Reduce tiberium at the mouse cell.
+ */
+class ReduceTiberiumCommandClass : public ViniferaCommandClass
+{
+    public:
+        ReduceTiberiumCommandClass() : ViniferaCommandClass() { IsDeveloper = true; }
+        virtual ~ReduceTiberiumCommandClass() {}
+
+        virtual const char *Get_Name() const override;
+        virtual const char *Get_UI_Name() const override;
+        virtual const char *Get_Category() const override;
+        virtual const char *Get_Description() const override;
+        virtual bool Process() override;
+
+        virtual KeyNumType Default_Key() const override { return KeyNumType(KN_NONE); }
+};
+
+
 #ifndef DEBUG
 /**
  *  Based class for all new developer/debug command classes.
