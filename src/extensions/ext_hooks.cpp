a5f3de88ee54e6b83ec30d1abd174cbc3d15552f
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-10-01 22:51:29 +0100

    Fixes a bug where spawned particles have an invalid particle system.

--- src/extensions/ext_hooks.cpp
+++ src/extensions/ext_hooks.cpp
@@ -89,6 +89,7 @@
 #include "bulletext_hooks.h"
 #include "terrainext_hooks.h"
 #include "superext_hooks.h"
+#include "particlesysext_hooks.h"
 
 #include "combatext_hooks.h"
 
@@ -178,6 +179,7 @@ void Extension_Hooks()
     BulletClassExtension_Hooks();
     TerrainClassExtension_Hooks();
     SuperClassExtension_Hooks();
+    ParticleSystemClassExtension_Hooks();
 
     CombatExtension_Hooks();
 
