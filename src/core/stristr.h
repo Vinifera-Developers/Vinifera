3757c79fc5bb0af9ced5cd4c30ef5f6096ce2d20
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-10-21 14:19:23 +0100

    Implements stristr.

---
+++ src/core/stristr.h
@@ -0,0 +1,33 @@
+/*******************************************************************************
+/*                 O P E N  S O U R C E  --  V I N I F E R A                  **
+/*******************************************************************************
+ *
+ *  @project       Vinifera
+ *
+ *  @file          STRISTR.H
+ *
+ *  @author        CCHyper
+ *
+ *  @brief         Case insensitive strstr implementation.
+ *
+ *  @license       Vinifera is free software: you can redistribute it and/or
+ *                 modify it under the terms of the GNU General Public License
+ *                 as published by the Free Software Foundation, either version
+ *                 3 of the License, or (at your option) any later version.
+ *
+ *                 Vinifera is distributed in the hope that it will be
+ *                 useful, but WITHOUT ANY WARRANTY; without even the implied
+ *                 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *                 PURPOSE. See the GNU General Public License for more details.
+ *
+ *                 You should have received a copy of the GNU General Public
+ *                 License along with this program.
+ *                 If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#pragma once
+
+#include "always.h"
+
+
+char *stristr(const char *str, const char *str_search);
