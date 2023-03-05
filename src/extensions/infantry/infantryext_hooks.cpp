e3eb4a1b15b86b8a36326aa1e0b94a5148547aa8
Rampastring <rampastring@outlook.com>
2023-03-05 22:00:06 +0200

    Fix bug where the game could access infantry DoControls out of bounds

--- src/extensions/infantry/infantryext_hooks.cpp
+++ src/extensions/infantry/infantryext_hooks.cpp
@@ -47,6 +47,7 @@
 #include "fatal.h"
 #include "debughandler.h"
 #include "asserthandler.h"
+#include "tibsun_inline.h"
 
 #include "hooker.h"
 #include "hooker_macros.h"
@@ -524,6 +525,30 @@ DECLARE_PATCH(_InfantryClass_Firing_AI_JumpJet_In_Air_Patch)
 }
 
 
+void _Set_Infantry_Facing_After_Doing_Check_For_Do_Nothing(InfantryClass* this_ptr)
+{
+    if (this_ptr->Doing == DO_NOTHING) {
+        return;
+    }
+
+    FacingType facing = this_ptr->Class->DoControls[this_ptr->Doing].Finish;
+    if (facing == FACING_NONE) {
+        return;
+    }
+
+    DirType dirtype = Facing_Dir(facing);
+    DirStruct ds = DirStruct(dirtype);
+    this_ptr->PrimaryFacing.Set(ds);
+}
+
+
+DECLARE_PATCH(_InfantryClass_Doing_AI_Fix_Invalid_Facing_Set)
+{
+    GET_REGISTER_STATIC(InfantryClass*, inf, esi);
+    _Set_Infantry_Facing_After_Doing_Check_For_Do_Nothing(inf);
+    JMP(0x004D8C14);
+}
+
 /**
  *  Main function for patching the hooks.
  */
@@ -533,6 +558,8 @@ void InfantryClassExtension_Hooks()
      *  Initialises the extended class.
      */
     InfantryClassExtension_Init();
+
+    Patch_Jump(0x004D8BE4, &_InfantryClass_Doing_AI_Fix_Invalid_Facing_Set);
 
     Patch_Jump(0x004D88FA, &_InfantryClass_Firing_AI_JumpJet_In_Air_Patch);
     Patch_Jump(0x004D8C83, &_InfantryClass_Doing_AI_JumpJet_Idle_Patch);
