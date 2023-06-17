9c73f1965d9ab62283e0de7ab5486d0d0ea97042
Rampastring <rampastring@outlook.com>
2023-06-17 21:30:03 +0300

    Give some bonus range for aircraft to work around poor unit chasing behaviour

--- src/extensions/technotype/technotypeext_hooks.cpp
+++ src/extensions/technotype/technotypeext_hooks.cpp
@@ -32,10 +32,39 @@
 #include "debughandler.h"
 #include "asserthandler.h"
 
+#include "hooker.h"
+#include "hooker_macros.h"
+
+
+/**
+ *  #issue-90
+ *
+ *  Disables the bugged bonus range for arcing projectiles.
+ *
+ *  Author: Rampastring
+ */
+DECLARE_PATCH(_TechnoTypeClass_In_Range_Disable_Arcing_Bonus_Range_Patch)
+{
+    // DTA addition: give some bonus extra range for aircraft so they don't waste time+
+    // and sometimes ammo when chasing enemy units. This often happens when an aircraft
+    // is told to attack a moving unit; the aircraft reaches firing range, stops to fire,
+    // the unit moves, the aircraft is suddenly out of range again and must move forward
+    // before it even managed to dispatch a single missile.
+    // And the process is repeated again and again.
+    // TODO make this a variable in Rules
+    GET_STACK_STATIC(TechnoTypeClass *, this_ptr, esp, 0x14);
+    if (this_ptr->What_Am_I() == RTTI_AIRCRAFTTYPE) {
+        _asm { add  edi, 0x200 } // 0x200 = 512 = 2 cells. Range is in leptons
+    }
+
+    JMP(0x0063D6AA);
+}
+
 
 /**
  *  Main function for patching the hooks.
  */
 void TechnoTypeClassExtension_Hooks()
 {
+    Patch_Jump(0x0063D5A7, &_TechnoTypeClass_In_Range_Disable_Arcing_Bonus_Range_Patch);
 }
