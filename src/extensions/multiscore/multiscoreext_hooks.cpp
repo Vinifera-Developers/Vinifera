8062e3ac60c978eedef4c2af96d2d6f89f31014d
Rampastring <rampastring@outlook.com>
2023-07-05 19:19:50 +0300

    Fix nonsensical economy score. #544

--- src/extensions/multiscore/multiscoreext_hooks.cpp
+++ src/extensions/multiscore/multiscoreext_hooks.cpp
@@ -29,15 +29,96 @@
 #include "debughandler.h"
 #include "asserthandler.h"
 
+#include "tibsun_globals.h"
+#include "house.h"
+#include "vector.h";
+
 #include "hooker.h"
 #include "hooker_macros.h"
 
+
+static int MostCreditsSpent;
+
+static void _MultiScore_Tally_Score_Get_Largest_CreditsSpent_Score()
+{
+    MostCreditsSpent = 0;
+
+    for (int i = 0; i < Houses.Count(); i++) {
+        if (Houses[i]->CreditsSpent > MostCreditsSpent) {
+            MostCreditsSpent = Houses[i]->CreditsSpent;
+        }
+    }
+}
+
+
+/**
+ *  #issue-544
+ *
+ *  Fixes the nonsensical economy stat in the score screen.
+ *  Records the highest "credits spent" score from all players for
+ *  later use in comparing players' economy scores.
+ *
+ *  @author: Rampastring
+ */
+DECLARE_PATCH(_MultiScore_Tally_Score_Fetch_Largest_CreditsSpent_Score)
+{
+    // static const void *address = (void*)0x007E1568;
+    _MultiScore_Tally_Score_Get_Largest_CreditsSpent_Score();
+
+    /**
+     *  Stolen bytes / code.
+     */
+    _asm { mov ecx, dword ptr ds:0x007E1568 }
+    JMP(0x005687AF);
+}
+
+
+/**
+ *  #issue-544
+ *
+ *  Fixes the nonsensical economy stat in the score screen.
+ *  Calculates a player's economy score based on their amount of
+ *  credits spent.
+ *
+ *  @author: Rampastring
+ */
+DECLARE_PATCH(_MultiScore_Tally_Score_Calculate_Economy_Score)
+{
+    GET_REGISTER_STATIC(HouseClass *, house, ebx);
+    static int economy_score;
+
+    // Calculate a percentage of how much Tiberium this house has
+    // harvested compared to the house that harvested the largest
+    // amount of credits during the match.
+    if (MostCreditsSpent > 0) {
+        if (house->CreditsSpent >= MostCreditsSpent) {
+            // For some reason the score screen presentation seems to
+            // lower this by some 1-2%, so we take that into account.
+            economy_score = 102;
+        }
+        else {
+            economy_score = (house->CreditsSpent * 100) / MostCreditsSpent;
+        }
+    } else {
+        economy_score = 0;
+    }
+
+    _asm { mov eax, [economy_score] };
+
+    /**
+     *  Assign economy score and continue score processing.
+     */
+    JMP_REG(ecx, 0x005689E0);
+}
 
 /**
  *  Main function for patching the hooks.
  */
 void MultiScoreExtension_Hooks()
 {
+    Patch_Jump(0x005687A9, &_MultiScore_Tally_Score_Fetch_Largest_CreditsSpent_Score);
+    Patch_Jump(0x005689D5, &_MultiScore_Tally_Score_Calculate_Economy_Score);
+
     /**
      *  #issue-187
      *  
