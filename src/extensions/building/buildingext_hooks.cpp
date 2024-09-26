cbbab84ff93188f0640ea6b1c301abc3814eed7c
ZivDero <kirill.andriiashin@gmail.com>
2024-09-26 07:45:58 +0300

    Remove accidental spaces

--- src/extensions/building/buildingext_hooks.cpp
+++ src/extensions/building/buildingext_hooks.cpp
@@ -64,13 +64,13 @@
 #include "hooker_macros.h"
 
 
- /**
-  *  A fake class for implementing new member functions which allow
-  *  access to the "this" pointer of the intended class.
-  *
-  *  @note: This must not contain a constructor or deconstructor!
-  *  @note: All functions must be prefixed with "_" to prevent accidental virtualization.
-  */
+/**
+ *  A fake class for implementing new member functions which allow
+ *  access to the "this" pointer of the intended class.
+ *
+ *  @note: This must not contain a constructor or deconstructor!
+ *  @note: All functions must be prefixed with "_" to prevent accidental virtualization.
+ */
 static class BuildingClassExt final : public BuildingClass
 {
 public:
