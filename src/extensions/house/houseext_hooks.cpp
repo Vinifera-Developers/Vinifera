ec0f1c8aaed2ec3847fb1ff054fa581c6f853f4a
Rami Pasanen <rampastring@outlook.com>
2024-10-09 00:39:52 +0300

    Add `TransformsInto` for UnitTypes

--- src/extensions/house/houseext_hooks.cpp
+++ src/extensions/house/houseext_hooks.cpp
@@ -35,6 +35,8 @@
 #include "super.h"
 #include "factory.h"
 #include "techno.h"
+#include "unittype.h"
+#include "unittypeext.h"
 #include "mouse.h"
 #include "fatal.h"
 #include "debughandler.h"
@@ -359,6 +361,116 @@ return_true:
 
 
 /**
+ *  #issue-715
+ *
+ *  Gets the number of queued objects when determining whether a cameo
+ *  should be disabled.
+ *
+ *  Author: Rampastring
+ */
+int _HouseClass_ShouldDisableCameo_Get_Queued_Count(FactoryClass* factory, TechnoTypeClass* technotype)
+{
+    int count = factory->Total_Queued(*technotype);
+    TechnoClass* factoryobject = factory->Get_Object();
+
+    if (factoryobject == nullptr || count == 0) {
+        return 0;
+    }
+
+    /**
+     *  Check that the factory is trying to create the object that the player is trying to queue
+     *  If not, we don't need to mess with the count
+     */
+    if (factoryobject->Techno_Type_Class() != technotype) {
+        return count;
+    }
+
+    /**
+    *  #issue-715
+    *
+    *  If the object can transform into another object through our special logic,
+    *  then check that doing so doesn't allow circumventing build limits
+    */
+    if (technotype->What_Am_I() == RTTI_UNITTYPE) {
+        UnitTypeClass* unittype = reinterpret_cast<UnitTypeClass*>(technotype);
+        UnitTypeClassExtension* unittypeext = Extension::Fetch<UnitTypeClassExtension>(unittype);
+
+        if (unittype->DeploysInto == nullptr && unittypeext->TransformsInto != nullptr) {
+            count += factory->House->UQuantity.Count_Of((UnitType)(unittypeext->TransformsInto->Get_Heap_ID()));
+        }
+    }
+
+    return count;
+}
+
+
+/**
+ *  #issue-715
+ *
+ *  Updates the build limit logic with unit queuing to
+ *  take our unit transformation logic into account.
+ */
+DECLARE_PATCH(_HouseClass_ShouldDisableCameo_BuildLimit_Fix)
+{
+    GET_REGISTER_STATIC(FactoryClass*, factory, ecx);
+    GET_REGISTER_STATIC(TechnoTypeClass*, technotype, esi);
+    static int queuedcount;
+
+    queuedcount = _HouseClass_ShouldDisableCameo_Get_Queued_Count(factory, technotype);
+
+    _asm { mov eax, [queuedcount] }
+    JMP_REG(ecx, 0x004CB77D);
+}
+
+
+/**
+ *  #issue-715
+ *
+ *  Take vehicles that can transform into other vehicles into acccount when
+ *  determining whether a build limit has been met/exceeded.
+ *  Otherwise these kinds of units could be used to bypass build limits
+ *  (build a limited vehicle, transform it, now you can build another vehicle).
+ *
+ *  Author: Rampastring
+ */
+DECLARE_PATCH(_HouseClass_Can_Build_BuildLimit_Handle_Vehicle_Transform)
+{
+    GET_REGISTER_STATIC(UnitTypeClass*, unittype, edi);
+    GET_REGISTER_STATIC(HouseClass*, house, ebp);
+    static UnitTypeClassExtension* unittypeext;
+    static int objectcount;
+
+    unittypeext = Extension::Fetch<UnitTypeClassExtension>(unittype);
+
+    /**
+     *  Stolen bytes / code.
+     */
+    objectcount = house->UQuantity.Count_Of((UnitType)unittype->Get_Heap_ID());
+
+    /**
+     *  Check whether this unit can deploy into a building.
+     *  If it can, increment the object count by the number of buildings.
+     */
+    if (unittype->DeploysInto != nullptr) {
+        objectcount += house->BQuantity.Count_Of((BuildingType)unittype->DeploysInto->Get_Heap_ID());
+    }
+    else if (unittypeext->TransformsInto != nullptr) {
+
+        /**
+         *  This unit can transform into another unit, increment the object count
+         *  by the number of transformed units.
+         */
+        objectcount += house->UQuantity.Count_Of((UnitType)(unittypeext->TransformsInto->Get_Heap_ID()));
+    }
+
+    _asm { mov esi, objectcount }
+
+continue_function:
+    JMP(0x004BC1B9);
+}
+
+
+/**
  *  Main function for patching the hooks.
  */
 void HouseClassExtension_Hooks()
@@ -373,4 +485,7 @@ void HouseClassExtension_Hooks()
 
     Patch_Jump(0x004BE200, &HouseClassExt::_Begin_Production);
     Patch_Jump(0x004BE6A0, &HouseClassExt::_Abandon_Production);
+
+    Patch_Jump(0x004CB777, &_HouseClass_ShouldDisableCameo_BuildLimit_Fix);
+    Patch_Jump(0x004BC187, &_HouseClass_Can_Build_BuildLimit_Handle_Vehicle_Transform);
 }
