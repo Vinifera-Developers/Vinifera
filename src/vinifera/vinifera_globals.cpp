f7b4d047f4e45655cd51c2563753d7c3043a7cc2
Alex <thehdotx@gmail.com>
2024-10-09 22:07:08 +0700

    skipping videos to speed up debug

--- src/vinifera/vinifera_globals.cpp
+++ src/vinifera/vinifera_globals.cpp
@@ -58,8 +58,8 @@ bool Vinifera_Developer_FrameStep = false;
 int Vinifera_Developer_FrameStepCount = 0;
 bool Vinifera_Developer_AIControl = false;
 
-bool Vinifera_SkipLogoMovies = false;
-bool Vinifera_SkipStartupMovies = false;
+bool Vinifera_SkipLogoMovies = true;
+bool Vinifera_SkipStartupMovies = true;
 
 bool Vinifera_NoTacticalVersionString = false;
 
