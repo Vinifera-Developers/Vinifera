57e16725d4f15217d472794d4f4fc49951245a57
ZivDero <kirill.andriiashin@gmail.com>
2025-03-05 02:31:31 +0300

    Fix damage to buildings

--- src/extensions/combat/combatext_hooks.cpp
+++ src/extensions/combat/combatext_hooks.cpp
@@ -438,7 +438,7 @@ void Vinifera_Explosion_Damage(const Coordinate& coord, int strength, TechnoClas
                 const Cell* list = object->Occupy_List();
                 distance = INT_MAX;
                 while (*list != REFRESH_EOL) {
-                    int trydist = Distance_Level_Snap(coord, Cell_Coord(*list++));
+                    int trydist = Distance_Level_Snap(coord, object->Get_Coord() + Cell_Coord(*list++));
                     distance = std::min(trydist, distance);
                 }
             } else {
