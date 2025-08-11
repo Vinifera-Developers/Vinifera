9d6b2549c3c2fcbaedef6d623630edcb2fed0d79
ZivDero <kirill.andriiashin@gmail.com>
2025-08-11 14:56:40 +0300

    Show a loading message box when loading from the client

--- src/spawner/spawner.cpp
+++ src/spawner/spawner.cpp
@@ -224,7 +224,8 @@ bool Spawner::Start_Scenario(char* scenario_name)
  */
 bool Spawner::Load_Game(const char* file_name)
 {
-    if (!file_name[0] || !::Load_Game(file_name)) {
+//    if (strlen(file_name) == 0 || !::Load_Game(file_name)) {
+    if (strlen(file_name) == 0 || !LoadOptionsClass().Load_File(file_name)) { // using LoadOptionsClass().Load_File here gives us a "Mission is loading. Please wait..." box.
         DEBUG_INFO("[Spawner] Failed to load savegame [%s]\n", file_name);
         MessageBox(MainWindow, Text_String(TXT_ERROR_LOADING_GAME), "Vinifera", MB_OK);
 
