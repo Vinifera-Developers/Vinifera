f297bcfae5250ea1a063f2aaf3f650b494a0b862
ZivDero <kirill.andriiashin@gmail.com>
2025-11-01 14:22:20 +0300

    Add crt debug flags

--- src/crt/crt_hooks.cpp
+++ src/crt/crt_hooks.cpp
@@ -62,6 +62,11 @@ DECLARE_PATCH(_set_fp_mode)
  */
 void CRT_Hooks()
 {
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+    _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+
     /**
      *  Call the games fpmath to make sure we init 
      */
