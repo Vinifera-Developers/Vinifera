67cc9aefd2b2345e298e3ea76c5ba8b78a28deef
ZivDero <kirill.andriiashin@gmail.com>
2025-11-01 22:24:00 +0300

    Guard memory flags into debug builds

--- src/crt/crt_hooks.cpp
+++ src/crt/crt_hooks.cpp
@@ -62,10 +62,12 @@ DECLARE_PATCH(_set_fp_mode)
  */
 void CRT_Hooks()
 {
+#ifdef _DEBUG
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
     _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
     _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+#endif
 
     /**
      *  Call the games fpmath to make sure we init 
