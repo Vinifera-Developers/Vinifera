6ae3777409fe86d76465cd1c1b2de78dfe758101
ZivDero <kirill.andriiashin@gmail.com>
2026-04-11 02:41:40 +0300

    Fix nonsense headers

--- src/new/movie/movieplayback_hooks.cpp
+++ src/new/movie/movieplayback_hooks.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
 /*                 O P E N  S O U R C E  --  V I N I F E R A                  **
 /*******************************************************************************
- *  @brief  Hooks for pluggable movie playback.
+ *  @brief  Hooks for movie playback.
  *
  *  SPDX-License-Identifier: GPL-3.0-or-later
  *  Copyright (c) 2020-2026 Vinifera contributors
--- src/new/movie/movieplayback_hooks.h
+++ src/new/movie/movieplayback_hooks.h
@@ -1,7 +1,7 @@
 /*******************************************************************************
 /*                 O P E N  S O U R C E  --  V I N I F E R A                  **
 /*******************************************************************************
- *  @brief  Declarations for the pluggable movie playback hooks.
+ *  @brief  Hooks for movie playback.
  *
  *  SPDX-License-Identifier: GPL-3.0-or-later
  *  Copyright (c) 2020-2026 Vinifera contributors
