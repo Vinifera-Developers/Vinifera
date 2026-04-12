eaa49934ddb8a63d81e09ff0556b896065d7672a
ZivDero <kirill.andriiashin@gmail.com>
2026-04-12 04:04:06 +0300

    Hooks fixes

--- src/new/movie/movieplayback_hooks.cpp
+++ src/new/movie/movieplayback_hooks.cpp
@@ -11,38 +11,11 @@
 
 #include "movieplayback_hooks.h"
 
-#include "ccfile.h"
-#include "debughandler.h"
 #include "hooker.h"
+#include "movie.h"
 #include "movieplayback.h"
-#include "playmovie.h"
 #include "syringe.h"
-#include "vinifera_globals.h"
-
-#include <cstdio>
-#include <string>
-
-
-static bool Vinifera_Play_Movie(const char *name, ThemeType theme, bool clear_before, bool stretch_allowed, bool clear_after)
-{
-    const std::string basename = Normalize_Movie_Basename(name);
-    if (basename.empty()) {
-        DEBUG_ERROR("Invalid movie filename \"%s\"!\n", name ? name : "<null>");
-        return false;
-    }
-
-    if (!MoviePlayback_Is_Available(basename.c_str())) {
-        return false;
-    }
-
-    return MoviePlayback_Play(basename.c_str(), theme, clear_before, stretch_allowed, clear_after);
-}
-
-
-static bool Vinifera_Play_Ingame_Movie(const char *name)
-{
-    return MoviePlayback_Play_Ingame(name);
-}
+#include "tspp.h"
 
 
 DEFINE_HOOK(0x00563677, _Play_Movie_Intercept_Patch, 5)
@@ -53,7 +26,7 @@ DEFINE_HOOK(0x00563677, _Play_Movie_Intercept_Patch, 5)
     GET_STACK(bool, stretch_allowed, 0x84);
     GET_STACK(bool, clear_before, 0x88);
 
-    if (Vinifera_Play_Movie(name, theme, clear_before, stretch_allowed, clear_after)) {
+    if (MoviePlayback_Play(name, theme, clear_before, stretch_allowed, clear_after)) {
         return 0x00563891;
     }
 
@@ -61,11 +34,11 @@ DEFINE_HOOK(0x00563677, _Play_Movie_Intercept_Patch, 5)
 }
 
 
-DEFINE_HOOK(0x00563AE7, _Play_Ingame_Movie_Create_Intercept_Patch, 5)
+DEFINE_HOOK(0x00563AE0, _Play_Ingame_Movie_Create_Intercept_Patch, 7)
 {
-    GET(const char *, name, ECX);
+    GET_STACK(const char *, name, 0x38);
 
-    if (Vinifera_Play_Ingame_Movie(name)) {
+    if (MoviePlayback_Play_Ingame(name)) {
         R->ESP(R->ESP() + 0x28);
         return 0x00563AF7;
     }
@@ -74,11 +47,11 @@ DEFINE_HOOK(0x00563AE7, _Play_Ingame_Movie_Create_Intercept_Patch, 5)
 }
 
 
-DEFINE_HOOK(0x00563C21, _Play_Ingame_Movie_VQType_Create_Intercept_Patch, 5)
+DEFINE_HOOK(0x00563C1C, _Play_Ingame_Movie_VQType_Create_Intercept_Patch, 5)
 {
-    GET(const char *, name, ECX);
+    auto& name = Make_Global<char[20]>(0x00806D74);
 
-    if (Vinifera_Play_Ingame_Movie(name)) {
+    if (MoviePlayback_Play_Ingame(name)) {
         R->ESP(R->ESP() + 0x28);
         return 0x00563C31;
     }
@@ -90,10 +63,9 @@ DEFINE_HOOK(0x00563C21, _Play_Ingame_Movie_VQType_Create_Intercept_Patch, 5)
 DEFINE_HOOK(0x005645D0, _Movie_Advance_Frame_Modern_Ingame_Patch, 5)
 {
     GET(VQHandle *, handle, ECX);
-    GET(bool *, done_ptr, EDX);
-    bool &done = *done_ptr;
+    GET(bool *, done, EDX);
 
-    const MoviePlaybackIngameAdvanceResult result = MoviePlayback_Advance_Ingame(handle, done);
+    const MoviePlaybackIngameAdvanceResult result = MoviePlayback_Advance_Ingame(handle, *done);
     if (result != MOVIEPLAYBACK_INGAME_NOT_HANDLED) {
         R->AL(result == MOVIEPLAYBACK_INGAME_FRAME_ADVANCED ? 1 : 0);
         return 0x005645DB;
@@ -103,12 +75,12 @@ DEFINE_HOOK(0x005645D0, _Movie_Advance_Frame_Modern_Ingame_Patch, 5)
 }
 
 
-DEFINE_HOOK(0x00564473, _Movie_Destroy_Modern_Ingame_Patch, 5)
+DEFINE_HOOK(0x00564470, _Movie_Destroy_Modern_Ingame_Patch, 5)
 {
     GET(VQHandle *, handle, ECX);
 
     if (MoviePlayback_Destroy_Ingame(handle)) {
-        return 0x005644A4;
+        return 0x005644A5;
     }
 
     return 0;
@@ -123,7 +95,12 @@ DEFINE_HOOK(0x00564610, _Movie_Pause_Modern_Ingame_Patch, 5)
         return 0x0056461F;
     }
 
-    return 0;
+    if (handle != nullptr && handle->VQA != nullptr) {
+        R->ECX(handle->VQA);
+        return 0x0056461A;
+    }
+
+    return 0x0056461F;
 }
 
 
@@ -135,7 +112,12 @@ DEFINE_HOOK(0x00564620, _Movie_Resume_Modern_Ingame_Patch, 5)
         return 0x0056462F;
     }
 
-    return 0;
+    if (handle != nullptr && handle->VQA != nullptr) {
+        R->ECX(handle->VQA);
+        return 0x0056462A;
+    }
+
+    return 0x0056462F;
 }
 
 
