cc6bd5d2a0186ca91cee9149bb73f164bfd269a1
ZivDero <kirill.andriiashin@gmail.com>
2026-04-21 04:04:00 +0300

    This is pointless now

--- src/extensions/scenario/scenarioext.cpp
+++ src/extensions/scenario/scenarioext.cpp
@@ -2950,7 +2950,6 @@ void ScenarioClassExtension::Create_Units(bool official)
          *  Assign the center of this house to the waypoint location.
          */
         hptr->Center = centroid.As_Coord();
-        Extension::Fetch(hptr)->Set_Spawn_Point(centroid);
         DEBUG_INFO("  Setting house center to %d,%d\n", centroid.X, centroid.Y);
 
         /**
